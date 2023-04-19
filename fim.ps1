9b2ec5109f9512b2f2ca4a09c3bffeb502c9901d
FebaRoy <91877998+FebaRoy@users.noreply.github.com>
2023-04-19 20:10:38 +0530

    Add files via upload

---
+++ fim.ps1
@@ -0,0 +1,123 @@
+﻿Function Calculate-File-Hash($filepath) {
+    $filehash = Get-FileHash -Path $filepath -Algorithm SHA512
+    return $filehash
+}
+
+Function Erase-Baseline-If-Already-Exists() {
+    $baselineExists = Test-Path -Path .\baseline.txt
+
+    if ($baselineExists) {
+        # Delete it
+        Remove-Item -Path .\baseline.txt
+    }
+}
+
+Function Restore-Files {
+    $restoreFileExists = Test-Path -Path "path of backup file"
+
+    if ($restoreFileExists) {
+        $restoreFiles = Get-Content -Path "path of backup file"
+
+        foreach ($file in $restoreFiles) {
+            $filePath = $file.Split("|")[0]
+            $fileContent = $file.Split("|")[1]
+
+            Set-Content -Path $filePath -Value $fileContent
+        }
+
+        Write-Host "Files have been restored successfully" -ForegroundColor Green
+    }
+    else {
+        Write-Host "Restore file not found" -ForegroundColor Red
+    }
+}
+
+Write-Host ""
+Write-Host "What would you like to do?"
+Write-Host ""
+Write-Host "    A) Collect new Baseline?"
+Write-Host "    B) Begin monitoring files with saved Baseline?"
+Write-Host ""
+$response = Read-Host -Prompt "Please enter 'A' or 'B'"
+Write-Host ""
+
+if ($response -eq "A".ToUpper()) {
+    # Delete baseline.txt if it already exists
+    Erase-Baseline-If-Already-Exists
+
+    # Calculate Hash from the target files and store in baseline.txt
+    # Collect all files in the target folder
+    $files = Get-ChildItem -Path .\Files
+
+    # For each file, calculate the hash, and write to baseline.txt
+    foreach ($f in $files) {
+        $hash = Calculate-File-Hash $f.FullName
+        "$($hash.Path)|$($hash.Hash)" | Out-File -FilePath .\baseline.txt -Append
+    }
+    
+}
+
+elseif ($response -eq "B".ToUpper()) {
+    
+    $fileHashDictionary = @{}
+
+    # Load file|hash from baseline.txt and store them in a dictionary
+    $filePathsAndHashes = Get-Content -Path .\baseline.txt
+    
+    foreach ($f in $filePathsAndHashes) {
+         $fileHashDictionary.add($f.Split("|")[0],$f.Split("|")[1])
+    }
+
+    # Begin (continuously) monitoring files with saved Baseline
+    while ($true) {
+        Start-Sleep -Seconds 1
+        
+        $files = Get-ChildItem -Path .\Files
+
+        # For each file, calculate the hash, and write to baseline.txt
+        foreach ($f in $files) {
+            $hash = Calculate-File-Hash $f.FullName
+            #"$($hash.Path)|$($hash.Hash)" | Out-File -FilePath .\baseline.txt -Append
+
+            # Notify if a new file has been created
+            if ($fileHashDictionary[$hash.Path] -eq $null) {
+                # A new file has been created!
+                Write-Host "$($hash.Path) has been created!" -ForegroundColor Green
+            }
+            else {
+
+                # Notify if a new file has been changed
+                if ($fileHashDictionary[$hash.Path] -eq $hash.Hash) {
+                    # The file has not changed
+                }
+                else {
+                    # File file has been compromised!, notify the user
+                    Write-Host "$($hash.Path) has changed!!!" -ForegroundColor Yellow
+
+                    # Restore the original file from the Restore.txt file
+                    $restoreFilePath = "path of backup file"
+                    if (Test-Path -Path $restoreFilePath) {
+                        $originalFileContent = Get-Content -Path $restoreFilePath
+                        Set-Content -Path $f.FullName -Value $originalFileContent
+                        Write-Host "Restored the original file from $restoreFilePath" -ForegroundColor Cyan
+                    }
+                    else {
+                        Write-Host "Could not restore the original file. Restore.txt not found." -ForegroundColor Red
+                    }
+
+                    # Update the file hash dictionary with the new hash
+                    $fileHashDictionary[$hash.Path] = $hash.Hash
+                }
+            }
+        }
+
+        # Check if any baseline file has been deleted
+        foreach ($key in $fileHashDictionary.Keys) {
+            $baselineFileStillExists = Test-Path -Path $key
+            if (-Not $baselineFileStillExists) {
+                # One of the baseline files must have been deleted, notify the user
+                Write-Host "$($key) has been deleted!" -ForegroundColor DarkRed -BackgroundColor Gray
+            }
+        }
+    }
+}
